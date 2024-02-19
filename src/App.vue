9148c420d4dd3aebacecda1be60a8ddb0b1eaa2e
GloriaBimbi <gloria.bimbi1@libero.it>
2024-02-19 12:27:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from "./store/index";
+import AppMain from "./components/AppMain.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: { AppMain, AppHeader, AppFooter },
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
