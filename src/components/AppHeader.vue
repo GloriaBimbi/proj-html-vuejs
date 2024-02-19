99766d9482c1f657f92eab782eb90cecca78d105
GloriaBimbi <gloria.bimbi1@libero.it>
2024-02-19 18:31:13 +0100

    fix AppHeader (feat hero)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,83 +16,187 @@ export default {
 
 <template>
   <header>
-    <div class="logo-container">
-      <img src="../assets/img/theme_eduprime_logo.png" alt="logo" />
+    <div class="header-bar">
+      <div class="logo-container">
+        <img src="../assets/img/theme_eduprime_logo.png" alt="logo" />
+      </div>
+      <div class="info">
+        <ul class="link-list">
+          <li v-for="link in links">
+            <a href="#">{{ link }}</a>
+            <font-awesome-icon icon="fa-solid fa-caret-down" />
+          </li>
+        </ul>
+        <button class="yellow-button">VIEW COURSES</button>
+      </div>
     </div>
-    <div class="info">
-      <ul class="link-list">
-        <li v-for="link in links">
-          <a href="#">{{ link }}</a>
-        </li>
-      </ul>
-      <button>VIEW COURSES</button>
+    <div class="hero-section">
+      <div class="close-menu">
+        <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+        <font-awesome-icon icon="fa-solid fa-book-open" />
+        <font-awesome-icon icon="fa-solid fa-gear" />
+      </div>
+      <h1>Key to your seccess</h1>
+      <span
+        >EduPrime is the most versatile WordPress theme for educational purposes
+        showcasing universities, courses, secondary schools etc.</span
+      >
+      <div class="buttons">
+        <button class="yellow-button">
+          <font-awesome-icon
+            class="button-icon"
+            icon="fa-solid fa-magnifying-glass"
+          />Search courses
+        </button>
+        <button class="white-button">
+          <font-awesome-icon
+            class="button-icon"
+            icon="fa-solid fa-user-plus"
+          />Search Apply for university
+        </button>
+      </div>
     </div>
+    <img src="../assets/img/Wave-1.png" alt="wave img" class="wave-img" />
   </header>
 </template>
 
 <style lang="scss" scoped>
 header {
-  height: 100px;
-  background-color: #da5f60e6;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  background-image: url("../assets/hero.png");
+  background-size: 100% 100%;
 
-  .logo-container {
-    img {
-      width: 200px;
-      padding-left: 40px;
-    }
-  }
+  color: white;
+  text-align: center;
+  position: relative;
 
-  .info {
-    height: 100%;
-
+  .header-bar {
+    height: 100px;
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
     align-items: center;
-    gap: 20px;
-    .link-list {
+
+    .logo-container {
+      img {
+        width: 200px;
+        padding-left: 40px;
+      }
+    }
+
+    .info {
       height: 100%;
 
       display: flex;
       justify-content: flex-end;
       align-items: center;
-      gap: 30px;
+      gap: 20px;
+      .link-list {
+        height: 100%;
 
-      li {
-        cursor: pointer;
-        padding: 38px 0;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        gap: 30px;
 
-        &:hover {
-          border-bottom: 6px solid #e9d657;
-        }
-
-        a {
-          color: white;
-          font-weight: bold;
-          font-size: 12px;
-
+        li {
+          cursor: pointer;
           &:hover {
             color: #e9d657;
           }
+
+          a {
+            color: white;
+            padding: 38px 0;
+            font-weight: bold;
+            font-size: 12px;
+            padding-right: 5px;
+            &:hover {
+              border-bottom: 6px solid #e9d657;
+              color: #e9d657;
+            }
+          }
+        }
+      }
+      .yellow-button {
+        padding: 15px 25px;
+        margin: 40px 40px 40px 0;
+        border: 0;
+        border-radius: 2rem;
+        color: white;
+        background-color: #e9d657;
+        font-weight: bold;
+        cursor: pointer;
+
+        &:hover {
+          background-color: #fedf10;
         }
       }
     }
-    button {
-      padding: 15px 25px;
-      margin: 40px 40px 40px 0;
-      border: 0;
-      border-radius: 2rem;
-      color: white;
-      background-color: #e9d657;
-      font-weight: bold;
-      cursor: pointer;
+  }
+  .hero-section {
+    min-height: 300px;
+    margin: 100px 350px 0 350px;
 
-      &:hover {
-        background-color: #fedf10;
+    .close-menu {
+      padding: 10px;
+      background-color: #e9d657;
+      border-radius: 5px;
+      box-shadow: 3px 3px 10px black;
+
+      position: absolute;
+      left: -5px;
+      top: 250px;
+
+      display: flex;
+      gap: 15px;
+      flex-direction: column;
+    }
+
+    h1 {
+      margin-bottom: 50px;
+      font-size: 40px;
+      font-weight: bolder;
+    }
+
+    span {
+      line-height: 30px;
+    }
+
+    .buttons {
+      .yellow-button,
+      .white-button {
+        padding: 15px 25px;
+        margin: 40px 40px 40px 0;
+        border: 0;
+        border-radius: 2rem;
+        font-weight: bold;
+        cursor: pointer;
+      }
+
+      .yellow-button {
+        color: white;
+        background-color: #e9d657;
+
+        &:hover {
+          background-color: #fedf10;
+        }
+      }
+
+      .white-button {
+        background-color: #faeaea;
+        color: #de6e70;
+
+        &:hover {
+          background-color: white;
+          color: #ff7174;
+        }
+      }
+      .button-icon {
+        margin-right: 10px;
       }
     }
+  }
+  .wave-img {
+    display: block;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// font awsome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faMagnifyingGlass,
+  faUserPlus,
+  faCaretDown,
+  faCartShopping,
+  faBookOpen,
+  faGear,
+} from "@fortawesome/free-solid-svg-icons";
+library.add(
+  faMagnifyingGlass,
+  faUserPlus,
+  faCaretDown,
+  faCartShopping,
+  faBookOpen,
+  faGear
+);
+
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
